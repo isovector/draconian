8cb230b632dade8642f6a7185c716b88a5f2625e
Sandy Maguire <sandy@sandymaguire.me>
2022-03-02 13:47:27 -0800

    Sanitize markdown files

--- draconian.sh
+++ draconian.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-set -x
-
-# curl http://web.archive.org/web/timemap/link/https://www2.gov.bc.ca/gov/content/covid-19/info/restrictions | grep -Eo 'http://.+/restrictions' > webmap
+curl http://web.archive.org/web/timemap/link/https://www2.gov.bc.ca/gov/content/covid-19/info/restrictions | grep -Eo 'http://.+/restrictions' > webmap
 
 mkdir html
 mkdir md
 
-# cat webmap | grep -Eo '[0-9]{14}' | grep -Eo '^.{8}' | uniq > days
+cat webmap | grep -Eo '[0-9]{14}' | grep -Eo '^.{8}' | uniq > days
 
 while read DAY; do
   URL=$(cat webmap | grep "/$DAY" | head -n1 )
@@ -15,3 +13,14 @@ while read DAY; do
   curl $URL | sed "s/${SLUG}//g" > html/${SLUG}.html
   pandoc -f html -t markdown -o md/${SLUG}.md html/${SLUG}.html &
 done < days
+
+for MD in md/*.md; do
+  sed -i '/^ *:::/d' $MD
+  sed -i '/div>/d' $MD
+  sed -i 's/\[[^\]*]\]{[^}]*}//' $MD
+  sed -i -n '/^!\[Share/q;p' $MD
+  sed -i '0,/{#main-content-anchor/d' $MD
+  sed -i 's/([/h#][^)]*)//g' $MD
+  sed -i 's/{[#.][^}]*}//g' $MD
+  sed -i 's/\[\]//g' $MD
+done
