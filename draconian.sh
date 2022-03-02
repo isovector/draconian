762d467507760e0e4eecb154ba5d68928e8e2414
Sandy Maguire <sandy@sandymaguire.me>
2022-03-02 14:55:22 -0800

    full script

--- draconian.sh
+++ draconian.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
+set -ex
+
 curl http://web.archive.org/web/timemap/link/https://www2.gov.bc.ca/gov/content/covid-19/info/restrictions | grep -Eo 'http://.+/restrictions' > webmap
 
 mkdir html
 mkdir md
 
-cat webmap | grep -Eo '[0-9]{14}' | grep -Eo '^.{8}' | uniq > days
+# cat webmap | grep -Eo '[0-9]{14}' | grep -Eo '^.{8}' | uniq > days
 
+rm slugs
 while read DAY; do
   URL=$(cat webmap | grep "/$DAY" | head -n1 )
   SLUG=$(echo $URL | grep -Eo '[0-9]{14}')
-  curl $URL | sed "s/${SLUG}//g" > html/${SLUG}.html
-  pandoc -f html -t markdown -o md/${SLUG}.md html/${SLUG}.html &
+  HTML="html/${SLUG}.html"
+
+  echo $SLUG >> slugs
+
+  curl -o $HTML $URL || curl -o $HTML $URL || curl -o $HTML $URL || curl -o $HTML $URL || curl -o $HTML $URL
+  sed -i "s/${SLUG}//g" $HTML
+
+  pandoc -f html -t markdown -o md/${SLUG}.md $HTML
+  sleep 2
 done < days
+
+find md -name '*' -size 1 -print0 | xargs -0 rm
 
 for MD in md/*.md; do
   sed -i '/^ *:::/d' $MD
@@ -24,3 +36,16 @@ for MD in md/*.md; do
   sed -i 's/{[#.][^}]*}//g' $MD
   sed -i 's/\[\]//g' $MD
 done
+
+mkdir res
+cd res
+git init
+cd ..
+
+while read SLUG; do
+  cp md/$SLUG.md res/pho.md
+  cd res
+  git add pho.md
+  git commit -m "Update for $SLUG"
+  cd ..
+done < slugs
