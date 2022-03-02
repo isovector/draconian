bcc4d8f601c92412c1c5cf6bb56e72e099d00468
Sandy Maguire <sandy@sandymaguire.me>
2022-03-02 13:24:49 -0800

    Only get one snapshot per day

--- draconian.sh
+++ draconian.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+set -x
+
 # curl http://web.archive.org/web/timemap/link/https://www2.gov.bc.ca/gov/content/covid-19/info/restrictions | grep -Eo 'http://.+/restrictions' > webmap
 
 mkdir html
 mkdir md
 
-while read URL; do
+# cat webmap | grep -Eo '[0-9]{14}' | grep -Eo '^.{8}' | uniq > days
+
+while read DAY; do
+  URL=$(cat webmap | grep "/$DAY" | head -n1 )
   SLUG=$(echo $URL | grep -Eo '[0-9]{14}')
-  curl $URL > html/${SLUG}.html
-  sleep 1
-done < webmap
+  curl $URL | sed "s/${SLUG}//g" > html/${SLUG}.html
+  pandoc -f html -t markdown -o md/${SLUG}.md html/${SLUG}.html &
+done < days
